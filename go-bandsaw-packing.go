ffb1df5b903d9c9d26b5bbe3f56edc9ba6b5174b
William Floyd <william.png2000@gmail.com>
2019-12-16 22:18:28 -0500

    Now working, all new. Next up is rotation of parts.

--- go-bandsaw-packing.go
+++ go-bandsaw-packing.go
@@ -1,52 +1,220 @@
 package packing
 
-// Rectangle holds a rectangle height, width, and ability to rotate
+// Rectangle ...
 type Rectangle struct {
-	Width      float32
-	Height     float32
-	Rotateable bool
+	Width  int
+	Height int
 }
 
-// Position holds X and Y coordinates
+// Position ...
 type Position struct {
-	X float32
-	Y float32
+	X int
+	Y int
 }
 
-// PlacedObject holds a rectangle and it's position, according to the center of the rectangle
+// PlacedObject ...
 type PlacedObject struct {
 	Rectangle Rectangle
 	Position  Position
-	Rotated   bool
 }
 
-// Solution holds a full set of placed rectangles
+// Solution ...
 type Solution struct {
 	PlacedObjects []PlacedObject
 	Boundary      Rectangle
 }
 
-// GetSolutions finds all possible solutions given boundaries and rectangles
-func GetSolutions(boundaries []Rectangle, rectangles []Rectangle, solutions *[]Solution, currentSolution *Solution) {
-	if canRectanglesFit(&boundaries, &rectangles) {
-
-	} else {
-		*solutions = append(*solutions, *currentSolution)
-	}
+// ObjectSet ...
+type ObjectSet struct {
+	Objects []Rectangle
 }
 
-// canRectanglesFit checks if a given set of rectangles and boundaries allows at least on more rectangle to fit.
-func canRectanglesFit(boundaries *[]Rectangle, rectangles *[]Rectangle) bool {
-	for _, rectangle := range *rectangles {
-		for _, boundary := range *boundaries {
-			if rectangle.Height <= boundary.Height && rectangle.Width <= boundary.Width {
-				return true
-			} else if rectangle.Rotateable {
-				if rectangle.Height <= boundary.Width && rectangle.Width <= boundary.Height {
-					return true
+// PlacedObjectSet ...
+type PlacedObjectSet struct {
+	PlacedObjects []PlacedObject
+}
+
+// BoundarySet ...
+type BoundarySet struct {
+	Boundaries []PlacedObject
+}
+
+// ParameterSet ...
+type ParameterSet struct {
+	Objects      ObjectSet
+	Boundaries   BoundarySet
+	CurrentState State
+}
+
+// State ...
+type State struct {
+	PlacedObjects PlacedObjectSet
+}
+
+// Solve ...
+func Solve(boundaries BoundarySet, rectangles ObjectSet, currentState State) []State {
+	parameterSets := []ParameterSet{}
+	for b, boundary := range boundaries.Boundaries {
+		for r, rectangle := range rectangles.Objects {
+			if rectangleFits(rectangle, boundary) {
+				objects := append([]Rectangle(nil), rectangles.Objects...)
+				objects[r] = objects[len(objects)-1]
+				objects = objects[:len(objects)-1]
+
+				bounds := append([]PlacedObject(nil), boundaries.Boundaries...)
+				bounds[b] = bounds[len(bounds)-1]
+				bounds = bounds[:len(bounds)-1]
+
+				state := currentState
+				state.PlacedObjects.PlacedObjects = append(state.PlacedObjects.PlacedObjects,
+					PlacedObject{
+						Rectangle: rectangle,
+						Position:  boundary.Position,
+					},
+				)
+
+				newBounds := SplitBoundary(boundary, rectangle)
+
+				if len(newBounds) == 0 {
+					parameterSets = append(parameterSets,
+						ParameterSet{
+							Objects: ObjectSet{
+								Objects: objects,
+							},
+							Boundaries:   BoundarySet{Boundaries: bounds},
+							CurrentState: state,
+						},
+					)
+				} else {
+					for _, boundarySet := range newBounds {
+						parameterSets = append(parameterSets,
+							ParameterSet{
+								Objects: ObjectSet{
+									Objects: objects,
+								},
+								Boundaries:   BoundarySet{Boundaries: append(boundarySet.Boundaries, bounds...)},
+								CurrentState: state,
+							},
+						)
+					}
 				}
 			}
+
 		}
 	}
+
+	if len(parameterSets) > 0 {
+
+		stateSet := []State{}
+
+		for _, parameterSet := range parameterSets {
+			stateSet = append(stateSet, Solve(parameterSet.Boundaries, parameterSet.Objects, parameterSet.CurrentState)...)
+		}
+
+		return stateSet
+
+	}
+
+	return []State{currentState}
+
+}
+
+func rectangleFits(candidate Rectangle, boundary PlacedObject) bool {
+	if candidate.Height <= boundary.Rectangle.Height && candidate.Width <= boundary.Rectangle.Width {
+		return true
+	}
 	return false
+}
+
+// SplitBoundary ...
+func SplitBoundary(boundary PlacedObject, rectangle Rectangle) []BoundarySet {
+	if boundary.Rectangle.Width == rectangle.Width && boundary.Rectangle.Height == rectangle.Height {
+		return []BoundarySet{}
+	} else if boundary.Rectangle.Width == rectangle.Width && boundary.Rectangle.Height != rectangle.Height {
+		return []BoundarySet{
+			BoundarySet{
+				Boundaries: []PlacedObject{
+					PlacedObject{
+						Rectangle: Rectangle{
+							Width:  boundary.Rectangle.Width,
+							Height: boundary.Rectangle.Height - rectangle.Height,
+						},
+						Position: Position{
+							X: boundary.Position.X,
+							Y: boundary.Position.Y + rectangle.Height,
+						},
+					},
+				},
+			},
+		}
+	} else if boundary.Rectangle.Height == rectangle.Height && boundary.Rectangle.Width != rectangle.Width {
+		return []BoundarySet{
+			BoundarySet{
+				Boundaries: []PlacedObject{
+					PlacedObject{
+						Rectangle: Rectangle{
+							Width:  boundary.Rectangle.Width - rectangle.Width,
+							Height: boundary.Rectangle.Height,
+						},
+						Position: Position{
+							X: boundary.Position.X + rectangle.Width,
+							Y: boundary.Position.Y,
+						},
+					},
+				},
+			},
+		}
+	}
+
+	return []BoundarySet{
+		BoundarySet{
+			Boundaries: []PlacedObject{
+				PlacedObject{
+					Rectangle{
+						Width:  boundary.Rectangle.Width - rectangle.Width,
+						Height: boundary.Rectangle.Height,
+					},
+					Position{
+						X: boundary.Position.X + rectangle.Width,
+						Y: boundary.Position.Y,
+					},
+				},
+				PlacedObject{
+					Rectangle{
+						Width:  rectangle.Width,
+						Height: boundary.Rectangle.Height - rectangle.Height,
+					},
+					Position{
+						X: boundary.Position.X,
+						Y: boundary.Position.Y + rectangle.Height,
+					},
+				},
+			},
+		},
+		BoundarySet{
+			Boundaries: []PlacedObject{
+				PlacedObject{
+					Rectangle{
+						Width:  boundary.Rectangle.Width,
+						Height: boundary.Rectangle.Height - rectangle.Height,
+					},
+					Position{
+						X: boundary.Position.X,
+						Y: boundary.Position.Y + rectangle.Height,
+					},
+				},
+				PlacedObject{
+					Rectangle{
+						Width:  boundary.Rectangle.Width - rectangle.Width,
+						Height: rectangle.Height,
+					},
+					Position{
+						X: boundary.Position.X + rectangle.Width,
+						Y: boundary.Position.Y,
+					},
+				},
+			},
+		},
+	}
+
 }
